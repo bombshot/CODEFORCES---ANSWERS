344ddbb70431538b3abb06d79d3872a651ab10ff
ARUN GUPTA <garun762@gmail.com>
2016-08-07 14:19:26 +0530

    Add files via upload

---
+++ coinsfixmoneymade.cpp
@@ -0,0 +1,71 @@
+//headerfile//////////////
+#include <bits/stdc++.h>
+//define///////////////////
+#define S(x) scanf("%d",&x)
+#define P(x) printf("%d\n",x)
+#define szo(x) ((int)sizeof(x))
+#define szs(x) ((int)(x).size())
+#define S2(x,y) scanf("%d%d",&x,&y)
+#define loop(i,a,b) for(int i = a; i < b; i++)
+#define revloop(i,a,n) for (int i=n-1;i>=a;i--)
+#define pi 3.14159265358979323846264338327950288419716939937510582097494459230781640
+//////typedef//////////
+typedef long long int ll;
+typedef unsigned long long int ull;
+const ll mod=1000000007;
+const int INF = 1000000000;
+using namespace std;
+//////functions////////
+ull pow2(ull s) {if(s!=0 && (s & (s-1)))return 0;else return 1;}
+ull powany(ll a,ll b) {ull res=1;a%=mod;for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ull prime(ull n) {if (n<=1)return 0;if(n<=3)return 1;if(n%2==0||n%3==0)return 0;for(ull i=5;i*i<=n;i+=6)if(n%(i)==0||n%(i+2)==0)return 0;return 1;}
+//head//////////////////
+bool ar[10000][10000];
+int main(){
+ios_base::sync_with_stdio(0);
+int n;
+cin>>n;
+int m;
+cin>>m;
+int arr[10000];
+for (int i = 1; i <=m; ++i)
+{
+       cin>>arr[i];
+       ar[i][0]=true;
+}
+for (int i = 1; i <=n; ++i)
+{
+      ar[0][i]=false;
+}
+ar[0][0]=true;
+for (int i = 1; i <=m; ++i)
+{
+    for(int j=1;j<=n;j++)
+    {
+       if (j<arr[i])
+       {//cout<<"H";
+           ar[i][j]=ar[i-1][j];
+       }
+       else
+       {
+          ar[i][j]=(ar[i-1][j] || ar[i-1][j-arr[i]]);
+          //cout<<"U";
+       }
+    }
+}
+if(ar[m][n])
+{
+    cout<<"yes";
+}
+else
+{
+    cout<<"no";
+}
+return 0;
+}
+
+/*
+
+coins are fixed not infinite
+
+*/
